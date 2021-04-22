c81aacc41aff9fca61ac41f99c16e312a8339769
rashad <omar_rashad@aol.com>
2021-04-22 11:43:33 +0200

    JOIN Answers

--- SqlZoo.sql
+++ SqlZoo.sql
@@ -62,3 +62,17 @@
 5.5) SELECT continent, COUNT(name) FROM world GROUP BY continent
 5.6) SELECT continent, COUNT(name) FROM world WHERE population >=10000000 GROUP BY continent
 5.7) SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 100000000
+
+6.0) SELECT matchid, player FROM goal WHERE teamid = 'GER'
+6.1) SELECT id,stadium,team1,team2 FROM game WHERE id = 1012
+6.2) SELECT player, teamid, stadium, mdate FROM game JOIN goal ON (game.id=goal.matchid) WHERE teamid= 'ger'
+6.3) SELECT team1, team2, player FROM game JOIN goal ON (game.id=goal.matchid) WHERE player LIKE 'Mario%'
+6.4) SELECT player, teamid, coach, gtime FROM goal JOIN eteam ON id = teamid WHERE gtime<=10
+6.5) SELECT mdate, teamname FROM game JOIN eteam ON eteam.id = game.team1 WHERE coach = 'Fernando Santos'
+6.6) SELECT player FROM goal JOIN game ON matchid = game.id WHERE stadium = 'National Stadium, Warsaw'
+6.7) SELECT DISTINCT player FROM goal JOIN game ON id = matchid WHERE (team1 = 'GER' OR team2 ='GER') AND (teamid <> 'GER')
+6.8) SELECT teamname, COUNT(teamid) AS Goals FROM goal JOIN eteam ON teamid = id GROUP BY teamname
+6.9) SELECT stadium, COUNT(stadium) FROM game JOIN goal ON id = matchid GROUP BY stadium
+9.10) SELECT id, mdate, COUNT(gtime) FROM game JOIN goal ON id = matchid WHERE (team1 = 'POL' OR team2='POL') GROUP BY id, mdate
+6.11) SELECT id, mdate, COUNT(*) FROM game JOIN goal ON id = matchid WHERE teamid = 'GER' GROUP BY id, mdate
+6.12) SELECT mdate,team1,SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1, team2, SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2 FROM game LEFT JOIN goal ON matchid = id GROUP BY mdate,id,team1,team2
